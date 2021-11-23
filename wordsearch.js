17a8df16ee5de189633b11913165bd880b6b297c
Abby <abby_faigao@yahoo.com>
2021-11-23 02:31:23 +0000

    add function and checksto the existing file from repo

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const verticalJoin = function(letters) {
+  const verticalLetters = [];
+  for (let col = 0; col < letters[0].length; col++) {
+    const tempArray = [];
+    for (let row = 0; row < letters.length; row++) {
+      tempArray.push(letters[row][col]);
+      verticalLetters.push(tempArray);
     }
-}
 
-module.exports = wordSearch
+    return verticalLetters.map((ls) => ls.join(''));
+  }
+};
+
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  if (verticalJoin(letters).includes(word)) {
+    return true;
+  }
+  const horizontalJoin = letters.map((ls) => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
+
